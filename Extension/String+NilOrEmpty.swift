ae046492a66e93edc69426dc37070a0da62baae6
이광용 <rhkdrmfh@gmail.com>
2018-07-12 13:43:54 +0900

    Update String+NilOrEmpty

--- Extension/String+NilOrEmpty.swift
+++ Extension/String+NilOrEmpty.swift
@@ -8,12 +8,30 @@
 
 import Foundation
 
-extension String {
-    func trim() -> String {
+protocol NilOrEmpty {
+    func trim() -> String?
+    func isNilOrEmpty() -> Bool
+}
+
+extension String: NilOrEmpty {
+    func trim() -> String? {
         return self.trimmingCharacters(in: CharacterSet.whitespaces).trimmingCharacters(in: CharacterSet.newlines)
     }
     func isNilOrEmpty()->Bool{
-        if(self.trim().isEmpty){return true}
+        guard let str = self.trim() else {return false}
+        if(str.isEmpty){return true}
+        else{return false}
+    }
+}
+
+extension Optional: NilOrEmpty where Wrapped == String {
+    func trim() -> String? {
+        guard let `self` = self else {return nil}
+        return self.trimmingCharacters(in: CharacterSet.whitespaces).trimmingCharacters(in: CharacterSet.newlines)
+    }
+    func isNilOrEmpty()->Bool{
+        guard let str = self.trim() else {return false}
+        if(str.isEmpty){return true}
         else{return false}
     }
 }
